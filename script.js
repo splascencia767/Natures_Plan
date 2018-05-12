5e681bec67918e65fad794338adb33daa3e3ac15
Unknown <splascencia767@west-mec.org>
2018-05-11 18:10:17 -0700

    Made a function to crop the files to the carousel
    Added function to auto crop the images in the carousel to squares so they can remain the same sizes.

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
+//Jquery
 $document.ready(function(){
     if(screen.width < 992){
-      $("img:odd").detach().class("")
-
+      $("img:odd").detach().
      }
   });
-
+//Poppers
 function assignPopper(){
   var 
 }  
+
+//Carousel Crop
+function cropImage(){
+  var imgs = document.getElementsByClassName("imgCrop");
+  for(var i = 0 ; i < imgs.length ; i++ ){
+   var l = img[i].height
+   var w = img[i].widht
+   cX1 = 0;
+   cX2 = w;
+   cY1 = 0;
+   cY2 = l;
+    if(l > w){
+      var xtra = l - w;
+      cY1 = (xtra/2);
+      cY2 = l - (xtra/2);
+    }else if(w > l){
+      var xtra = w - l;
+      cX1 = (xtra/2);
+      cX2 = w - (xtra/2);
+    }
+
+    img[i].styles.clip = "rect("+cX1+"px,"+cX2 +"px,"+cY2+"px,"+cY1+"px)"
+  };
+
+}
+
+
+//EventListeners
+window.addEventListener("load", cropImage)
